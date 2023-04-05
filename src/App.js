41e4086d1ac7623428dec1aa6fc983f12415ca29
byiringiroscar <byiringoroscar@gmail.com>
2023-04-05 20:33:27 +0200

    Pass the data by props

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import './App.css';
 import Todo from './component/todo';
 
 function App() {
+  const todos = [
+    {id: 1, title: 'WASH DISHES', completed: false},
+    {id: 1, title: 'WASH Dinee', completed: true}
+  ]
   return (
     <div className="App">
-      <Todo />
+      {todos.map((todo) => {
+        return (<Todo todo={todo} />)
+      })}
     </div>
   );
 }
--- src/component/todo.js
+++ src/component/todo.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 
-const Todo = () => (<h1 data-testid="todo-1">Hello world</h1>);
+const Todo = ({ todo }) => {
+    const { id, title, completed }  = todo;
+    const h1 = <h1>{title}</h1>
+    const text = completed ? <strike>{h1}</strike> : h1
+    return (
+        <h1 data-testid="todo-1">{text}</h1>
+    );
+};
 
 
 export default Todo;
